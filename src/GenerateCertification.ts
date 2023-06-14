b7d3c97e0008c1e3bd0d2da448fce1b191635f20
Jonatas Braz de Sousa <jonatas.sousa@cofen.gov.br>
2023-06-14 17:24:31 -0300

    feat: ajustes de posicionamento

--- src/GenerateCertification.ts
+++ src/GenerateCertification.ts
@@ -1,31 +1,25 @@
 import fs from 'fs';
 import PDFDocument from 'pdfkit';
-import { IParticipantData, LoadData } from './LoadData';
+import { LoadData } from './LoadData';
 
 export class GenerateCertification {
   async execute() {
     const load_data = new LoadData();
-    load_data
-      .execute('src/data/data.csv')
-      .then((participants: IParticipantData[]) => {
-        participants.map((participant) => {
-          const IMAGE = fs.readFileSync('src/images/certificado.jpg');
-          const pdf = new PDFDocument({
-            layout: 'landscape',
-          });
-
-          pdf.image(IMAGE, -56, -25, { width: 900, height: 662 });
-          pdf.moveDown(16);
-          pdf
-            .font('Times-Roman')
-            .fontSize(25)
-            .fillColor('#025999')
-            .text(`${participant.name}`, { align: 'center' });
-          pdf.pipe(
-            fs.createWriteStream(`src/export-pdf/${participant.cpf}.pdf`),
-          );
-          pdf.end();
-        });
+    const data = await load_data.execute('src/data/data.csv');
+    const IMAGE = fs.readFileSync('src/images/certificado.jpg');
+    data.map(async (participant) => {
+      const pdf = new PDFDocument({
+        layout: 'landscape',
       });
+      pdf.image(IMAGE, -56, -25, { width: 900, height: 662 });
+      pdf.moveDown(16);
+      pdf
+        .font('Times-Roman')
+        .fontSize(25)
+        .fillColor('#022777')
+        .text(`${participant.name}`, { align: 'center' });
+      pdf.pipe(fs.createWriteStream(`src/export-pdf/${participant.cpf}.pdf`));
+      pdf.end();
+    });
   }
 }
